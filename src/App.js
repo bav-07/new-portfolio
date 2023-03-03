dd077e79090e918cc1bc4980c7a336af902925ed
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-03-03 22:27:18 +0000

    test 2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import TilesHeader from './containers/TilesHeader';
 function App() {
   return (
     <>
-      <div className='absolute top-0 left-0 w-[100vw] h-[100vh] -z-10'>
+      <div className='absolute top-0 left-0 w-[100vw] h-[100vh]'>
         <TilesHeader/>
       </div>
-      <div className='w-fit'>
-        <h1 className='text-white w-fit mx-auto h-[10px] my-auto pointer-events-none'>Bav</h1>
+      <div className=''>
+        <h1></h1>
       </div>
     </>
   );
