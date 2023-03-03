a0c0e208401eec910e82c0e47c1659201729da96
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-03-03 22:27:09 +0000

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className='absolute top-0 left-0 w-[100vw] h-[100vh] -z-10'>
         <TilesHeader/>
       </div>
-      <div className='w-[100vw]'>
+      <div className='w-fit'>
         <h1 className='text-white w-fit mx-auto h-[10px] my-auto pointer-events-none'>Bav</h1>
       </div>
     </>
